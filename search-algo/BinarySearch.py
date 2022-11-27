fb06a24fb539ccf2fabd7a96e7d24d86fd43eee6
archit-github <118385940+archit-github@users.noreply.github.com>
2022-11-27 15:19:27 +0000

    binary and linear done

---
+++ search-algo/BinarySearch.py
@@ -0,0 +1,43 @@
+'''
+time complexity : O(nlogn)
+simple logic 
+find middle then check the value and repeat
+middle = floor(low+ (high-low)/2)
+[low,high)
+'''
+import math
+
+def BinarySearch(haystack: list, needle: int) -> bool:
+    high = len(haystack)
+    low = 0
+    while low < high:
+        middle = math.floor(low + (high - low)//2)
+        # print(middle)
+        if haystack[middle] == needle:
+            return True
+        elif haystack[middle] > needle:
+            high = middle
+        else:
+            low = middle + 1
+    return False
+
+
+if __name__ == "__main__":
+    l = [21, 23, 3132, 24211]
+    print("first test cases::")
+    print(BinarySearch(l, 21))
+    print(BinarySearch(l, 23))
+    print(BinarySearch(l, 3132))
+    print(BinarySearch(l, 24211))
+    print(BinarySearch(l, 231))
+    print(BinarySearch(l, 6757))
+    print(BinarySearch(l, 20))
+    print("second test cases::")
+    lo = [1, 3, 5, 6, 7]
+    print(BinarySearch(lo, 1))
+    print(BinarySearch(lo, 3))
+    print(BinarySearch(lo, 5))
+    print(BinarySearch(lo, 6))
+    print(BinarySearch(lo, 7))
+    print(BinarySearch(lo, 75))
+    print(BinarySearch(lo, 9))
--- search-algo/LinearSearch.py
+++ search-algo/LinearSearch.py
@@ -1,3 +1,20 @@
 '''
-
+time complexity : O(N)
+iterate over each value and return.
 '''
+
+def LinearSearch(array: list, value: int) -> bool:
+    for i in array:
+        if i == value:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    l = [31,123,231,213,123,321]
+    print(LinearSearch(l, 3))
+    print(LinearSearch(l, 31))
+    print(LinearSearch(l, 123))
+    print(LinearSearch(l, 332))
+    print(LinearSearch(l, 323))
+    print(LinearSearch(l, 321))
